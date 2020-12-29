c8afbf0cbba51d077ec68d3e6c16da3178dda302
hilmansulaeman <hilmansulaeman0504@gmail.com>
2020-12-29 23:38:44 +0700

    first commit

---
+++ js/js.js
@@ -0,0 +1,11 @@
+//Custom Script //
+
+$(document).ready(function() {
+    // Owl Carousel Slider
+    $("#banner-section .owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        nav:true
+    });
+});
